1af75ceb2d5ba203227a285d85267014dde62435
Raghav Deivachilai <raghavd@Raghavs-MacBook-Pro.local>
2021-02-07 19:39:29 -0500

    small changes

--- data-app/src/app/fda-data/fda-data.component.ts
+++ data-app/src/app/fda-data/fda-data.component.ts
@@ -13,23 +13,16 @@ import {SocialAuthService} from 'angularx-social-login';
 })
 export class FdaDataComponent implements OnInit {
   username: string = null;
-  newColor1: boolean = false;
-  newColor2: boolean = false;
-  newColor3: boolean = false;
-  newColor4: boolean = false;
   pageEvent: PageEvent;
-  nameflag: boolean = false;
-  idflag: boolean = false;
-  stateflag: boolean = false;
-  partyflag: boolean = false;
   candidatenameval: string = null;
   candidateidval: string = null;
   stateval: string = null;
   partyval: string = null;
-  candidatestatusval: string = null;
   dataSource: Candidatedata = null;
   displayedColumns: string[] = ['candidate_id', 'party', 'name', 'candidate_inactive', 'has_raised_funds', 'incumbent_challenge', 'state', 'district_number', 'office', 'candidate_status'];
   states: string[] = ['ID', 'Party', 'State', 'Name'];
+  displayflags: boolean[] = [false, false, false, false];
+  colorflags: boolean[] = [false, false, false, false];
 
   constructor(private fecService: FecserviceService, private route: ActivatedRoute, private authService: SocialAuthService, private router: Router) {
   }
@@ -58,7 +51,6 @@ export class FdaDataComponent implements OnInit {
 
   //Filter by candidate name
   findByCandidateName(candidatename: string) {
-    console.log(candidatename);
     this.fecService.getCandidateName(1, 20, candidatename).pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
@@ -66,7 +58,6 @@ export class FdaDataComponent implements OnInit {
 
   //Filter by candidate ID
   findByCandidateID(candidateid: string) {
-    console.log(candidateid);
     this.fecService.getCandidateID(1, 20, candidateid).pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
@@ -74,110 +65,91 @@ export class FdaDataComponent implements OnInit {
 
   //Filter by candidate state
   findByState(state: string) {
-    console.log(state);
     this.fecService.getState(1, 20, state).pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Filter by candidate party name
   findByPartyName(partyname: string) {
-    console.log(partyname);
     this.fecService.getPartyName(1, 20, partyname).pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   // Sort based on candidate ID
   sortcandidateid() {
     this.fecService.Sort(1, 20, 'candidate_id').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Sort based on the aprty of the candidate
   sortcandidateparty() {
     this.fecService.Sort(1, 20, 'party').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Sort based on the candidate state
   sortcandidatestate() {
     this.fecService.Sort(1, 20, 'state').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Sort by candidate name
   sortcandidatename() {
     this.fecService.Sort(1, 20, 'name').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Sort descending by candidate  ID
   sortcandidateiddesc() {
     this.fecService.Sortdesc(1, 20, 'candidate_id').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Sort descending by party name of the candidate
   sortcandidatepartydesc() {
     this.fecService.Sortdesc(1, 20, 'party').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Sort descending by state of the candidate
   sortcandidatestatedesc() {
     this.fecService.Sortdesc(1, 20, 'state').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Sort descending by name of the candidate
   sortcandidatenamedesc() {
     this.fecService.Sortdesc(1, 20, 'name').pipe(
       map((candidate_data: Candidatedata) => this.dataSource = candidate_data)
     ).subscribe();
   }
+
   //Signout user
   signOut(): void {
     localStorage.removeItem('status');
     this.authService.signOut();
     this.router.navigate(['login']);
   }
+
   //UI changes
   displayform(id: number) {
-    if (id === 1) {
-      this.nameflag = !this.nameflag;
-      this.newColor1 = !this.newColor1;
-      this.newColor2 = false;
-      this.newColor3 = false;
-      this.newColor4 = false;
-      this.idflag = false;
-      this.stateflag = false;
-      this.partyflag = false;
-    } else if (id === 2) {
-      this.nameflag = false;
-      this.idflag = false;
-      this.stateflag = !this.stateflag;
-      this.newColor2 = !this.newColor2;
-      this.newColor1 = false;
-      this.newColor3 = false;
-      this.newColor4 = false;
-      this.partyflag = false;
-    } else if (id === 3) {
-      this.nameflag = false;
-      this.idflag = false;
-      this.stateflag = false;
-      this.partyflag = !this.partyflag;
-      this.newColor3 = !this.newColor3;
-      this.newColor1 = false;
-      this.newColor2 = false;
-      this.newColor4 = false;
-    } else if (id === 4) {
-      this.nameflag = false;
-      this.idflag = !this.idflag;
-      this.newColor4 = !this.newColor4;
-      this.newColor1 = false;
-      this.newColor2 = false;
-      this.newColor3 = false;
-      this.stateflag = false;
-      this.partyflag = false;
+    this.displayflags[id] = !this.displayflags[id];
+    this.colorflags[id] = !this.colorflags[id];
+    for (let idval = 0; idval < 4; idval++) {
+      if (idval === id) {
+        continue;
+      }
+      this.displayflags[idval] = false;
+      this.colorflags[idval] = false;
     }
   }
 }
